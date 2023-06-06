3055dee4113f7b0d7b9f0939f64b97a84c0833f0
gsdutra <gabrielsprecherdutra@gmail.com>
2023-06-06 13:27:55 -0300

    fix: exit menus fixed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,11 @@
 import ThemeSwitch from "./ThemeSwitch"
 import Link from 'next/link';
+import 'animate.css';
 
 export default function Menu(props: any){
-	return (
-	<div className={`z-10 h-screen w-[100%] bg-black bg-opacity-50 absolute top-0 left-0 ${props.show?"":"hidden"}`}>
-		<div className="h-screen w-[40%] bg-white absolute top-0 right-0"
-		onClick={()=>props.hide(false)}/>
-		<div className="h-screen w-[60%] max-w-[350px] bg-light dark:bg-dark absolute top-0 left-0 z-20 p-14">
+	return (<>
+	<div className={`z-10 h-screen w-[100%] bg-black bg-opacity-50 absolute top-0 left-0 ${props.show?"":"hidden"}`} onClick={()=>props.hide(false)}/>
+	<div className={`h-screen w-[60%] max-w-[350px] bg-light dark:bg-dark absolute top-0 left-0 z-20 p-14 ${props.show?"":"hidden"}`}>
 			<Link href='/chats'>
 				Chats
 			</Link> <br/><br/>
@@ -22,6 +21,5 @@ export default function Menu(props: any){
 			Esquema de cores: <br/>
 			<ThemeSwitch/>
 		</div>
-	</div>
-	)
+	</>)
 }
--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,7 +1,9 @@
+import 'animate.css';
+
 export default function Menu(props: any){
-	return (
-	<div className={`z-10 h-screen w-[100%] bg-black bg-opacity-50 absolute top-0 left-0 ${props.show?"":"hidden"}`} onClick={()=>props.hide(false)}>
-		<div className="h-screen w-[60%] max-w-[350px] bg-light dark:bg-dark absolute top-0 right-0 z-20 p-14">
+	return (<>
+	<div className={`z-10 h-screen w-[100%] bg-black bg-opacity-50 absolute top-0 left-0 ${props.show?"":"hidden"}`} onClick={()=>props.hide(false)}/>
+	<div className={`h-screen w-[60%] max-w-[350px] bg-light dark:bg-dark absolute top-0 right-0 z-20 p-14 ${props.show?"":"hidden"} fade-in-out`}>
 
 			<button className='button bt bg-blue w-[240px]'>
 				<a>CRIAR CONTA</a>
@@ -11,6 +13,5 @@ export default function Menu(props: any){
 				<a>FAZER LOGIN</a>
 			</button>
 		</div>
-	</div>
-	)
+	</>)
 }
